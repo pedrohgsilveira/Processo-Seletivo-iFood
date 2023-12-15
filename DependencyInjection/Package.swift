013db731ef402347bc8b30f436a3dc941467b393
pedrohgsilveira <49411884+pedrohgsilveira@users.noreply.github.com>
2023-12-15 12:17:07 -0300

    Add dependecy injection (#7)

---
+++ DependencyInjection/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DependencyInjection",
+    products: [
+        .library(name: "DependencyInjection", targets: ["DependencyInjection"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.4")
+    ],
+    targets: [
+        .target(name: "DependencyInjection", dependencies: ["Swinject"]),
+        .testTarget(name: "DependencyInjectionTests", dependencies: ["DependencyInjection"])
+    ]
+)
